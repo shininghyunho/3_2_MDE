bdd016fef89f185ce90fb02cfe12bf023c05c033
shininghyunho <46080945+shininghyunho@users.noreply.github.com>
2019-12-24 15:00:00 +0900

    hyunho

---
+++ cs_uos/1024실습자료/putty실습관련자료/jkit-1/default/dep/main.o.d
@@ -0,0 +1,46 @@
+main.o: ../main.c \
+  c:/program\ files/winavr-20100110/lib/gcc/../../avr/include/avr/io.h \
+  c:/program\ files/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  c:/program\ files/winavr-20100110/lib/gcc/../../avr/include/inttypes.h \
+  c:/program\ files/winavr-20100110/lib/gcc/../../avr/include/stdint.h \
+  c:/program\ files/winavr-20100110/lib/gcc/../../avr/include/avr/iom128.h \
+  c:/program\ files/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h \
+  c:/program\ files/winavr-20100110/lib/gcc/../../avr/include/avr/common.h \
+  c:/program\ files/winavr-20100110/lib/gcc/../../avr/include/avr/version.h \
+  c:/program\ files/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h \
+  c:/program\ files/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h \
+  c:/program\ files/winavr-20100110/lib/gcc/../../avr/include/avr/interrupt.h \
+  c:/program\ files/winavr-20100110/lib/gcc/../../avr/include/stdio.h \
+  c:\program\ files\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stdarg.h \
+  c:\program\ files\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h \
+  c:/program\ files/winavr-20100110/lib/gcc/../../avr/include/string.h
+
+c:/program\ files/winavr-20100110/lib/gcc/../../avr/include/avr/io.h:
+
+c:/program\ files/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h:
+
+c:/program\ files/winavr-20100110/lib/gcc/../../avr/include/inttypes.h:
+
+c:/program\ files/winavr-20100110/lib/gcc/../../avr/include/stdint.h:
+
+c:/program\ files/winavr-20100110/lib/gcc/../../avr/include/avr/iom128.h:
+
+c:/program\ files/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h:
+
+c:/program\ files/winavr-20100110/lib/gcc/../../avr/include/avr/common.h:
+
+c:/program\ files/winavr-20100110/lib/gcc/../../avr/include/avr/version.h:
+
+c:/program\ files/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h:
+
+c:/program\ files/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h:
+
+c:/program\ files/winavr-20100110/lib/gcc/../../avr/include/avr/interrupt.h:
+
+c:/program\ files/winavr-20100110/lib/gcc/../../avr/include/stdio.h:
+
+c:\program\ files\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stdarg.h:
+
+c:\program\ files\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h:
+
+c:/program\ files/winavr-20100110/lib/gcc/../../avr/include/string.h:
---
+++ cs_uos/1024실습자료/putty실습관련자료/jkit-1/x.c
@@ -0,0 +1,7 @@
+main()
+{
+    int i;
+    unsigned char x;
+    x = 255;
+    for( i = 0 ; i < x ; i ++) printf("i = %d\n",i);
+}
